44de42cfd582766bdf284afc12947fe856b18a8e
unknown <nickoluoch1130@gmail.com>
2024-05-08 16:04:05 +0300

    organizing code

--- Objects-objects-constructor/index.js
+++ Objects-objects-constructor/index.js
@@ -1,7 +1,42 @@
 const btn = document.querySelector('.btn');
+const txt = document.querySelector('.text');
+const body = document.querySelector('body');
+
+
 
 btn.addEventListener('click',()=>{
-    alert('Hello my glorious king!!!')
+   const player1 = new Player('Aaron','x');
+   const player3 = new Player('WanBisakaGodon','o');
+   const player4 = new Player('Harry','The Lord Maguire')
+    printName(player1);
+    printName(playerTwo)
+    printName(player3)
+    player4.sayName();
 })
 
-console.log('hELLO');
+
+function printName(player){
+    const text = document.createElement('div');
+    text.textContent = player.name;
+    body.appendChild(text);
+}
+
+function Player(name,marker){
+    this.name = name;
+    this.marker = marker;
+    this.sayName = ()=> {
+        txt.textContent = `${name},${marker}`
+    }
+}
+
+const playerOne = {
+    name: "tim",
+    marker: "X"
+  };
+  
+  const playerTwo = {
+    name: "jenn",
+    marker: "O"
+  };
+
+  
