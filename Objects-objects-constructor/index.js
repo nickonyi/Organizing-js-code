0591a2698bb585544cd6e6ee4cf31b2ba26f2e38
unknown <nickoluoch1130@gmail.com>
2024-05-16 09:52:06 +0300

    code

--- Objects-objects-constructor/index.js
+++ Objects-objects-constructor/index.js
@@ -41,4 +41,53 @@ const playerOne = {
 
 
 
+  function Books (title, author, pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = ()=> {
+        return `The ${title} by ${author}, ${pages} pages, ${read}`
+    }
+  }
+
+  const book1 = new Books('Osama bin Laden','Walela Omwanga',345,'just read it!');
+  
+  console.log(book1.info());
+
+
+  function Person(name) {
+    this.name = name;
+  }
+  
+  Person.prototype.sayName = function() {
+    console.log(`Hello, I'm ${this.name}!`);
+  };
+  
+  function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+  }
+  
+  // Don't do this!
+  // Use Object.setPrototypeOf(Player.prototype, Person.prototype)
+  Object.setPrototypeOf(Player.prototype,Person.prototype);
+  
+  function Enemy(name) {
+    this.name = name;
+    this.marker = '^';
+  }
+  
+  // Not again!
+  // Use Object.setPrototypeOf(Enemy.prototype, Person.prototype)
+ Object.setPrototypeOf(Enemy.prototype,Person.prototype);
+  
+  Enemy.prototype.sayName = function() {
+    console.log('HAHAHAHAHAHA');
+  };
+  
+  const carl = new Player('carl', 'X');
+  const dath = new Enemy('Darth','^^&')
+  carl.sayName();
+  dath.sayName(); // Uh oh! this logs "HAHAHAHAHAHA" because we edited the sayName function!
   
