24148de780217e5b9393251ecd490b23f168e8e3
nickonyi <nickoluoch1130@gmail.com>
2026-01-09 13:46:01 +0300

    created a folder on factory functions

--- factory-functions/app.js
+++ factory-functions/app.js
@@ -2,10 +2,3 @@ function createUser(name) {
   const discordName = "@" + name;
   return { name, discordName };
 }
-
-const user1 = createUser("dan");
-console.log(user1.discordName);
-
-const obj = { a: 1, b: 2 };
-const { a, b } = objg;
-console.log([a, b]);
